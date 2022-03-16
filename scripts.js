7757cb75ae75db1304f2149a625df6cccdb0ae83
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-03-16 19:25:34 +0300

    done

--- scripts.js
+++ scripts.js
@@ -3,3 +3,11 @@ console.log(16)
 console.log('%c انا في الصف الحادي عشر' , 'color: green')
 
 // جافاسكربت ليست جافا
+
+let badget = 20
+
+console.log(badget > 30 && badget < 80)
+
+badget = 20 + 30
+
+console.log(badget > 30 && badget < 80)
