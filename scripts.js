664c2c7c3d17f639256210bc0978c89c45d2b95f
Abdalmohsen16 <100500608+Abdalmohsen16@users.noreply.github.com>
2022-03-16 22:22:48 +0300

    done

--- scripts.js
+++ scripts.js
@@ -11,3 +11,41 @@ console.log(budget > 30 && budget < 80)
 budget += 30
 
 console.log(budget > 30 && budget < 80)
+
+let grade = prompt('Enter your grade');
+
+if(grade >= 90){
+    console.log('لقد حصلت على امتياز 🥳')
+} else if(grade >= 80){
+    console.log('لقد حصلت على جيد جداً🤩')
+} else if(grade >= 70){
+    console.log('لقد حصلت على جيد🙂')
+} else if(grade >= 60){
+    console.log('لقد حصلت على مقبول😕')
+} else if(grade >= 50){
+    console.log('لقد حصلت على ضعيف☹️')
+} else {
+    console.log('"راسب💔')
+}
+
+let grade1 = prompt("Enter your grade");
+switch (true) {
+    case (grade1 >= 90):
+        console.log('لقد حصلت على امتياز 🥳');
+        break;
+    case (grade1 >= 80):
+        console.log('لقد حصلت على جيد جداً🤩');
+        break;
+    case (grade1 >= 70):
+        console.log('لقد حصلت على جيد🙂');
+        break;
+    case (grade1 >= 60):
+        console.log('لقد حصلت على مقبول😕')
+        break;
+    case (grade1 >= 50):
+        console.log('لقد حصلت على ضعيف☹️')
+        break;
+    default:
+        console.log('"راسب💔');
+        break;
+}
